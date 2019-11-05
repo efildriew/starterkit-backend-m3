2e0e6f6248cc69c8655378bceb7aa9edcf8ca91e
Marc Valles Casares <13a1911@gmail.com>
2019-11-05 14:01:21 +0100

    created journeys model

---
+++ models/Journeys.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const journeySchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    location: {
+      type: [Number],
+      required: true,
+    },
+    destinations: {
+      type: [[Number]],
+      required: true,
+    },
+    journeyDate: Date,
+    users: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    },
+  },
+);
+
+const Journey = mongoose.model('Journey', journeySchema);
+
+module.exports = Journey;
